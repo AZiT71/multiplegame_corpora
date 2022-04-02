4a83f592152f43b7f5b882747d61ca260c8579f7
AZiT <azamat-khasanov0@yandex.ru>
2022-04-02 18:43:33 +0300

    add tags in dataset add reading all files in dir

--- src/dlg_parser.py
+++ src/dlg_parser.py
@@ -1,6 +1,7 @@
 import codecs
 import re
 
+
 def parse_input(input_file):
     parsed_dlg = []
     infile = codecs.open(input_file, "r", "utf-8")
@@ -9,31 +10,41 @@ def parse_input(input_file):
         parsed_q = quest_str.split("\r\n", 0)
         parsed_q = re.sub(r'\r\n$', '', parsed_q[0])
         datapoint = parsed_q
-        if (datapoint != ''): parsed_dlg.append(datapoint)
+        if datapoint != '':
+            parsed_dlg.append(datapoint)
     return parsed_dlg
 
-def line_to_data (dataset, line, exeption_info):
+
+def line_to_data(dataset, line, exception_info, short_name, ord_num, dlg_size):
     datapoint = {}
     try:
-        if not(re.match(r"^\[.*\]$", line)):
+        if not (re.match(r"^\[.*]$", line)):
             datapoint['Comment_flg'] = 'N'
-            splitted = line.split(': ', 1)
-            datapoint['Speaker'] = splitted[0]
-            datapoint['Text'] = re.sub(r"^\(.*\)\s", '', splitted[1])
+            separated = line.split(': ', 1)
+            datapoint['Speaker'] = separated[0]
+            datapoint['Text'] = re.sub(r"^\(.*\)\s", '', separated[1])
             try:
-                action = re.findall(r"^\(.*\)", splitted[1])[0]
+                action = re.findall(r"^\(.*\)", separated[1])[0]
                 datapoint['Action'] = action
-            except:
+            except Exception:
                 datapoint['Action'] = ''
             dataset.append(datapoint)
         else:
+
             datapoint['Comment_flg'] = 'Y'
             datapoint['Text'] = line
             datapoint['Speaker'] = ''
             datapoint['Action'] = ''
             dataset.append(datapoint)
-    except:
-        exeption_info.append(line)
-        print('Bad line exeption:')
-        print('Line text: ', line)
-        print('====================================================')
+        datapoint['Row_ID'] = short_name + str(ord_num)
+        if id != 1:
+            datapoint['Prev_Row_ID'] = short_name + str(ord_num - 1)
+        else:
+            datapoint['Prev_Row_ID'] = ''
+        if id != dlg_size:
+            datapoint['Next_Row_ID'] = short_name + str(ord_num + 1)
+        else:
+            datapoint['Next_Row_ID'] = ''
+    except Exception:
+        exception_info.append(line)
+        print('Bad line exception:\nLine text: ', line, '\n==========================\n')
--- src/main.py
+++ src/main.py
@@ -1,20 +1,33 @@
 import dlg_parser
 import csv
+from os import listdir
+from os.path import isfile, join
 
-file_path = "C:/Users/Tim-c/PycharmProjects/multiplegame_corpora/data/src/dead_space_dlg.txt"
-dialog = dlg_parser.parse_input(file_path)
+dlg_dir = "data/src/"
 dataset = []
 bad_lines = []
-for line in dialog:
-    dlg_parser.line_to_data(dataset, line, bad_lines)
+short_name = ''
+ord_num = 0
+g_names = [f for f in listdir(dlg_dir) if isfile(join(dlg_dir, f))]
+for g_name in g_names:
+    dialog = dlg_parser.parse_input(dlg_dir+g_name)
+    for word in g_name.split('_'):
+        if word != 'dlg.txt':
+            short_name += word[0]
+        else:
+            short_name += '-'
+    for line in dialog:
+        ord_num += 1
+        dlg_parser.line_to_data(dataset, line, bad_lines, short_name, ord_num, len(dialog))
 
-outfile = open("C:/Users/Tim-c/PycharmProjects/multiplegame_corpora/data/Out/dataset_dead_space.csv", "w")
-writer = csv.DictWriter(outfile, ['Speaker', 'Text', 'Action', 'Comment_flg'])
+
+outfile = open("data/Out/dataset_dead_space.csv", "w")
+writer = csv.DictWriter(outfile, ['Row_ID', 'Speaker', 'Text', 'Action', 'Comment_flg', 'Prev_Row_ID', 'Next_Row_ID'])
 writer.writeheader()
 writer.writerows(dataset)
 outfile.close()
 
-err_log_file = open("C:/Users/Tim-c/PycharmProjects/multiplegame_corpora/data/Out/_ERR_dataset_dead_space.txt", "w")
+err_log_file = open("data/Out/_ERR_dataset_dead_space.txt", "w")
 for err_line in bad_lines:
     err_log_file.write(err_line)
     err_log_file.write("\r\n=====================================\r\n")
